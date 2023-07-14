50c2a102fce2abdd43e810739f11943eec36fd5e
Rises007 <132374139+Rises007@users.noreply.github.com>
2023-07-14 05:36:30 +0530

    Update dns_client3.cpp

--- chat program using dns/dns_client3.cpp
+++ chat program using dns/dns_client3.cpp
@@ -1,5 +1,5 @@
 //g++ dns_client3.cpp -o dns_client3 -pthread
-//./dns_client3
+//./dns_client3 "name of user"
 #include <bits/stdc++.h>
 #include <unistd.h>
 #include <string.h>
@@ -11,21 +11,21 @@
 #include<pthread.h>
 
 #define MAXLINE 1024
-#define IP "127.0.0.1"
+#define IP "127.0.0.1"  //this is for local hosting , we can also add server ip
 
 using namespace std;
 struct emp {
-	int a;
+	int a; //this is identifier used for identifying packate type on the server side
 	struct node 
 	{
 		char i[256]; // source id 
 		char msg[256]; //ip
-		int p;
+		int p; //port number
 		char con_user[256]; //connetcted user destination id
 	} node1;
-	char chat[256];
+	char chat[256]; //this is the chat or message
 };
-int query(string id) {
+int query(string id) {   //this function gives the random port number on which the user was registered first time assigned by the os
 	struct emp s1;
 	s1.a = 2;
 	strcpy(s1.node1.con_user, id.c_str());
@@ -48,10 +48,7 @@ int query(string id) {
 	return s2.node1.p;
 
 }
-void con_msg(int port ) {
-		
-	//	int port = query(id);
-//cout<<"port = "<<port<<endl;
+void con_msg(int port ) {    //this function acts as server from the client side so that it can message from other client and see the chat!!
 	int sockfd;
 	struct sockaddr_in servaddr;
 	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -76,13 +73,13 @@ cout<<"server listening port"<<port<<endl;
 	
 }
 
-void call2(int port) {
+void call2(int port) {   //this is for keeping the server of client always in running state
 	while(1) {
 		con_msg(port);
 	}
 }
 	
-struct emp Send_msg(void *s1) {	
+struct emp Send_msg(void *s1) {	// this function is for receiving the node information from actual dns server about the other client to whom sending message
         int sockfd;
 	    struct sockaddr_in	 servaddr;
 
@@ -100,12 +97,12 @@ struct emp Send_msg(void *s1) {
 	len = sizeof(servaddr);
 	recvfrom(sockfd, &s2, sizeof(struct emp), MSG_WAITALL, ( struct sockaddr *) &servaddr, &len);
 	close(sockfd);
-	return s2;	
+	return s2;	//returing the target client's details packate
 }
 
 
-void func2(string id) {
-	    int sockfd;
+void func2(string id) {  //this function has two major functionality one is calling send_msg() for retreving other client ip port etc info and other is
+	    int sockfd;  								// establishing a socket connection with the target client  
 		while(1) {
 		struct emp temp;
 		string ch;
@@ -116,12 +113,11 @@ void func2(string id) {
 		strcpy(temp.chat, ch.c_str());
 		struct emp temp2;
 		temp2 = Send_msg(&temp);
-		if (temp2.node1.p == 0) {
+			
+		if (temp2.node1.p == 0) {           //if the dns returns a packate with port 0 it means the target client is not registered in the server
 			cout<<"invalid user"<<endl;
 		} else {
-
-		
-		struct sockaddr_in	 servaddr;
+		struct sockaddr_in	 servaddr;   // here we are  establishing connection with the other client  
 
     	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 	
@@ -139,19 +135,14 @@ cout<<"dest port = "<<temp2.node1.p<<endl;
 		
 		
 }
-
-
-int main(int argc, char **argv) {
-	string id = "";
-	for (int i =1; i < argc; i++) {
-		id = id + argv[i];
-	}
+void register_user(string id)  //this is for registering user first time
+{
 	int sockfd;
-	struct sockaddr_in	 servaddr;
-    struct emp s1;
+	struct sockaddr_in	 servaddr;   //here we are sending a packate to the server for registering 
+   	 struct emp s1;
 	strcpy(s1.node1.i, id.c_str());
 	s1.a = 1;
-    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 	
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = inet_addr(IP);
@@ -159,12 +150,22 @@ int main(int argc, char **argv) {
 	
 	sendto(sockfd, &s1, sizeof(struct emp), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr));
 	close(sockfd);
-	int port = query(id);
+	
+}
+int main(int argc, char **argv) {   //here we are passing command line argument for getting the client user id while running the code
+	string id = "";
+	for (int i =1; i < argc; i++) {   //storing in a string
+		id = id + argv[i];
+	}
+	register_user(id);  //calling function for regsiter
+	
+	int port = query(id);  //callling query() for getting the main port on which this user was registered
+	
 	cout<<"main port = "<<port<<endl;
-	thread th1(func2, id);
+	thread th1(func2, id);  //threading the function so that we can use run several function at a same time
 	thread th2(call2, port);
 	
-	th2.join();
+	th2.join();   //calling the threading function same time
 	th1.join();
 	
 	return 0;
